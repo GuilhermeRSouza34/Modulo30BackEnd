5363204d4758c5a40d07eeefcd4617e9bf85e4d4
Guilherme <guilhermersouza34@gmail.com>
2024-02-27 22:47:04 -0300

    Exercicio Desenvolvido para o Modulo 30 do curso de Desenvolvedor Full Stack Java

---
+++ mod30/ExemploVendasMod30/src/main/java/br/com/rpires/dao/ClienteDAO.java
@@ -0,0 +1,80 @@
+package br.com.rpires.dao;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import br.com.rpires.dao.generic.GenericDAO;
+import br.com.rpires.domain.Cliente;
+
+public class ClienteDAO extends GenericDAO<Cliente, Long> implements IClienteDAO {
+
+	public ClienteDAO() {
+		super();
+	}
+
+	@Override
+	public Class<Cliente> getTipoClasse() {
+		return Cliente.class;
+	}
+
+	@Override
+	protected String getQueryInsercao() {
+		return "INSERT INTO TB_CLIENTE (ID, NOME, CPF, TEL, ENDERECO, NUMERO, CIDADE, ESTADO, ESTADO_CIVIL) VALUES (nextval('sq_cliente'),?,?,?,?,?,?,?,?)";
+	}
+
+	@Override
+	protected String getQueryExclusao() {
+		return null;
+	}
+
+	@Override
+	protected void setParametrosQueryInsercao(PreparedStatement stmInsert, Cliente entity) throws SQLException {
+		stmInsert.setString(1, entity.getNome());
+		stmInsert.setLong(2, entity.getCpf());
+		stmInsert.setLong(3, entity.getTel());
+		stmInsert.setString(4, entity.getEnd());
+		stmInsert.setLong(5, entity.getNumero());
+		stmInsert.setString(6, entity.getCidade());
+		stmInsert.setString(7, entity.getEstado());
+		stmInsert.setString(8, entity.getEstadoCivil());
+	}
+
+	@Override
+	protected void setParametrosQueryExclusao(PreparedStatement stmDelete, Long valor) throws SQLException {
+
+	}
+
+	@Override
+	protected String getQueryAtualizacao() {
+		return "UPDATE TB_CLIENTE SET NOME = ?, TEL = ?, ENDERECO = ?, NUMERO = ?, CIDADE = ?, ESTADO = ?, ESTADO_CIVIL = ? WHERE CPF = ?";
+	}
+
+	@Override
+	protected void setParametrosQueryAtualizacao(PreparedStatement stmUpdate, Cliente entity) throws SQLException {
+		stmUpdate.setString(1, entity.getNome());
+		stmUpdate.setLong(2, entity.getTel());
+		stmUpdate.setString(3, entity.getEnd());
+		stmUpdate.setLong(4, entity.getNumero());
+		stmUpdate.setString(5, entity.getCidade());
+		stmUpdate.setString(6, entity.getEstado());
+		stmUpdate.setString(7, entity.getEstadoCivil());
+		stmUpdate.setLong(8, entity.getCpf());
+	}
+
+	@Override
+	protected void setParametrosQuerySelect(PreparedStatement stmSelect, Long valor) throws SQLException {
+		stmSelect.setLong(1, valor);
+	}
+
+	@Override
+	public void atualiarDados(Cliente entity, Cliente entityCadastrado) {
+		entityCadastrado.setCidade(entity.getCidade());
+		entityCadastrado.setCpf(entity.getCpf());
+		entityCadastrado.setEnd(entity.getEnd());
+		entityCadastrado.setEstado(entity.getEstado());
+		entityCadastrado.setNome(entity.getNome());
+		entityCadastrado.setNumero(entity.getNumero());
+		entityCadastrado.setTel(entity.getTel());
+		entityCadastrado.setEstadoCivil(entity.getEstadoCivil());
+	}
+}
